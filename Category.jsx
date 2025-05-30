e1f5cba9fb6ee4fffb38fcc1756076a1433ea373
Tanupandey28 <67185714+Tanupandey28@users.noreply.github.com>
2025-05-30 21:21:31 +0530

    Add files via upload

---
+++ Category.jsx
@@ -0,0 +1,48 @@
+import { TiThSmallOutline } from "react-icons/ti";
+import { MdFreeBreakfast } from "react-icons/md";
+import { TbSoupFilled } from "react-icons/tb";
+import { CiBowlNoodles } from "react-icons/ci";
+import { MdOutlineFoodBank } from "react-icons/md";
+import { GiFullPizza } from "react-icons/gi";
+import { GiHamburger } from "react-icons/gi";
+
+
+    const Categories=[
+    {
+        id:1,
+        name:"All",
+        icon:<TiThSmallOutline className="w-[60px] h-[60px] text-green-600" />
+    },
+     {
+        id:2,
+        name:"Breakfast",
+        icon:<MdFreeBreakfast className="w-[60px] h-[60px] text-green-600" />
+    },
+     {
+        id:3,
+        name:"Soup",
+        icon:<TbSoupFilled className="w-[60px] h-[60px] text-green-600" />
+    },
+    {
+        id:4,
+        name:"Pasta",
+        icon:<CiBowlNoodles className="w-[60px] h-[60px] text-green-600" />
+    },
+     {
+        id:5,
+        name:"Main Course",
+        icon:<MdOutlineFoodBank className="w-[60px] h-[60px] text-green-600" />
+    },
+     {
+        id:6,
+        name:"Pizza",
+        icon:<GiFullPizza className="w-[60px] h-[60px] text-green-600" />
+    },
+     {
+        id:7,
+        name:"Burger",
+        icon:<GiHamburger className="w-[60px] h-[60px] text-green-600" />
+    }
+]
+
+export default Categories;
